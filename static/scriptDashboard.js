39286385d01d79c0e9d1ae788d1d59f25afc82ca
Alejandro Portillo <die.portillo01@gmail.com>
2021-04-19 15:02:24 -0600

    bregando durisimo pana

--- static/scriptDashboard.js
+++ static/scriptDashboard.js
@@ -1,8 +1,7 @@
-
-
 function peakTasks(){
     document.getElementById("listUsers").style.display = "none";
     document.getElementById("input-Date-Box-Hidden").style.display = "block";
+    document.getElementById("opcionRemoverUsuarios").style.display = "none";
 }
 
 function peakUsers(){
@@ -12,9 +11,13 @@ function peakUsers(){
     document.getElementById("listToDo").style.display = "none";
 
     document.getElementById("listUsers").style.display = "block";
-
+    document.getElementById("container-Input-Eliminar").style.display = "none";
+    
+    
     cleanUsers();
     listUsers(getUsersFromLocalStorage());
+
+    document.getElementById("opcionRemoverUsuarios").style.display = "block";
 }
 
 function cleanUsers(){
@@ -53,7 +56,8 @@ function getUsersFromLocalStorage(){
 }
 
 function listUsers(usersArray){
-    let table, row, cell1, cell2, cell3, arrUsers = [], currentUser = {};
+    let table, row, cell1, cell2, currentUser = {},
+        button;
 
     table = document.getElementById("usersList-w3");
 
@@ -62,14 +66,59 @@ function listUsers(usersArray){
 
         cell1 = row.insertCell(0);
         cell2 = row.insertCell(1);
-        cell3 = row.insertCell(2);
     
         cell1.innerHTML = currentUser.correo;
         cell2.innerHTML = currentUser.usuario;
-        cell3.innerHTML = "ELIMINAR";
     }
 
 }
 
+function listUsersToDelete(){
+    let select, option, currentUser = {}, arrUsers = [],
+        valor = "";
+    document.getElementById("container-Input-Eliminar").style.display = "block";
+    select = document.getElementById("user");
+
+    if (localStorage.getItem("wUserArray") !== null) {
+        arrUsers = JSON.parse(localStorage.getItem("wUserArray"));
+    }
+
+    for(currentUser of arrUsers){
+
+        if("cliente" === currentUser.rol){
+            option = document.createElement("option");
+            option.text = currentUser.usuario;
+            option.value = currentUser.correo;
+            select.add(option, select[-1]);
+        } else {
+            continue;
+        }
+        
+    }
+
+}
+
+function deleteUserCliente(){
+    let correo = "", arrUsers = [], currentUser = {}, arrResult = [];
+
+    correo = document.getElementById("user").value;
+
+    if (localStorage.getItem("wUserArray") !== null) {
+        arrUsers = JSON.parse(localStorage.getItem("wUserArray"));
+    }
+
+    for(currentUser of arrUsers){
+
+        if(correo !== currentUser.correo){
+            arrResult.push(currentUser);
+        } else {
+            continue;
+        }
+    }
+
+    localStorage.setItem("wUserArray", JSON.stringify(arrResult));
+    cleanValues("user");
+    peakUsers();
+}
 
 w3.includeHTML();
