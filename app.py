8879d3529b7b4b7dfb12729527e2ee9bee95ad6d
Diego Portillo <65975109+DAP-web@users.noreply.github.com>
2021-04-18 23:04:29 -0600

    preparando rey

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ app.config["CORS_HEADERS"] = "Content-Type"
 def home():
     return render_template("index.html")
 
-# @app.route("/index2")
-# def index2():
-#     return render_template("index2.html")
-
 @app.route("/calendar")
 def calendar():
     return render_template("calendar.html")
